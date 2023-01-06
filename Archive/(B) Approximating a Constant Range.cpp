e05dc67e31a377507e9f314d52a01fce595f1860
Dan <57308957+D4NZ-jpg@users.noreply.github.com>
2023-01-06 13:08:06 -0600

    Update

---
+++ Archive/(B) Approximating a Constant Range.cpp
@@ -0,0 +1,62 @@
+// Problem: B. Approximating a Constant Range
+// Contest: Codeforces - Codeforces Round #333 (Div. 2)
+// URL: https://codeforces.com/problemset/problem/602/B
+// Memory Limit: 256 MB
+// Time Limit: 2000 ms
+// Start: 05-01-2023 14:13:55
+// End: 05-01-2023 14:53:58
+// Duration: 0:40:03
+// Rating: 1400
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define gcd(x, y) __gcd(x, y)
+#define mcm(x, y) abs(x* y) / gcd(x, y)
+#define sz(x)     (int)(x).size()
+#define all(x)    begin(x), end(x)
+
+int main() {
+	cin.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+
+	int n;
+	cin >> n;
+	int nums[n];
+	for (int i = 0; i < n; i++) cin >> nums[i];
+
+	if (n == 2) {
+		cout << 2;
+		return 0;
+	}
+
+	int iMax = 0;
+	int l = 0, r = -1;
+	int a = -1, b = -1;
+	int lpa, lpb;
+
+	while (r < n - 1) {
+		r++;
+		if (a == -1) a = nums[r];
+		if (b == -1 && a != nums[r]) b = nums[r];
+		if (nums[r] == a) lpa = r;
+		if (nums[r] == b) lpb = r;
+		if (a == -1 || b == -1) continue;
+		if (nums[r] != a && nums[r] != b) {
+			bool ca = abs(b - nums[r]) == 1;
+			if (ca) {
+				l   = lpa + 1;
+				a   = nums[r];
+				lpa = r;
+			} else {
+				l   = lpb + 1;
+				b   = nums[r];
+				lpb = r;
+			}
+		};
+
+		iMax = max(iMax, r - l + 1);
+	}
+	cout << max(iMax, r - l + 1);
+	return 0;
+}
---
+++ Archive/(B) More Cowbell.cpp
@@ -0,0 +1,50 @@
+// Problem: B. More Cowbell
+// Contest: Codeforces - Codeforces Round #334 (Div. 2)
+// URL: https://codeforces.com/problemset/problem/604/B
+// Memory Limit: 256 MB
+// Time Limit: 2000 ms
+// Start: 05-01-2023 14:57:47
+// End: 05-01-2023 15:52:28
+// Duration: 0:54:41
+// Rating: 1400
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define gcd(x, y) __gcd(x, y)
+#define mcm(x, y) abs(x* y) / gcd(x, y)
+#define sz(x)     (int)(x).size()
+#define all(x)    begin(x), end(x)
+
+int main() {
+	cin.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+
+	int n, k;
+	cin >> n >> k;
+	int nums[n];
+	for (int i = 0; i < n; i++) cin >> nums[i];
+	int imax = 0;
+	if (n < k * 2) imax = nums[n - 1];
+	for (int i = 0; i < n - (k * 2 - n); i++)
+		imax = max(nums[i] + nums[n - (k * 2 - n) - i - 1], imax);
+	cout << imax;
+	return 0;
+}
+
+// This works when you can put as many items in a box as you
+// want as long as it doesn't exceed s
+/*
+int n, k;
+cin >> n >> k;
+int      imax = 0;
+uint64_t sum  = 0;
+
+for (int i = 0; i < n; i++) {
+    int j;
+    cin >> j;
+    imax = max(j, imax);
+    sum += j;
+}
+cout << max(imax,(int)ceil(sum / (double)k));
+*/
