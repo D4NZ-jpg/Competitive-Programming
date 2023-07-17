36f20308efeccc668f09bccbac9874ca86e1ea0f
Danz <57308957+D4NZ-jpg@users.noreply.github.com>
2023-07-17 14:18:28 -0600

    Update Template

--- Settings/template.cpp
+++ Settings/template.cpp
@@ -1,11 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+using uint = unsigned int;
+
 #define gcd(x, y) __gcd(x, y)
-#define mcm(x, y) abs(x* y) / gcd(x, y)
-#define sz(x)     (int)(x).size()
+#define mcm(x, y) abs((x) * (y)) / gcd(x, y)
 #define all(x)    begin(x), end(x)
 #define pb(x)     push_back(x)
+#define endl      '\n'
 
 int main() {
 	cin.tie(nullptr);
