295ca3c212f687666017452e3f3e0d89441f77e5
Danz <57308957+D4NZ-jpg@users.noreply.github.com>
2023-12-06 11:06:49 -0600

    Template: Added pair of longs macro

--- Settings/template.cpp
+++ Settings/template.cpp
@@ -10,16 +10,19 @@ using namespace std;
 
 using ll  = long long;
 using ull = unsigned long long;
+using pll = pair<ll, ll>;
 
 #define gcd(x, y) __gcd(x, y)
 #define mcm(x, y) abs((x) * (y)) / gcd(x, y)
 #define all(x)    begin(x), end(x)
 #define pb(x)     push_back(x)
 #define endl      '\n'
+
 int main() {
 #ifdef DEBUG
 	std::cout << std::unitbuf;
 #endif
+
 	cin.tie(nullptr);
 	ios_base::sync_with_stdio(false);
 
